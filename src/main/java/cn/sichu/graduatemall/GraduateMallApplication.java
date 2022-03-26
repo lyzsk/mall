e038f733f130a3a76c44948bcca54958f6f5ed78
lyzsk <sichu.huang2021@outlook.com>
2022-03-26 15:12:53 +0000

    2022/03/26

---
+++ src/main/java/cn/sichu/graduatemall/GraduateMallApplication.java
@@ -0,0 +1,13 @@
+package cn.sichu.graduatemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduateMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduateMallApplication.class, args);
+	}
+
+}
