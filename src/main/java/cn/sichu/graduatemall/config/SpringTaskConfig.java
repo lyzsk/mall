e88cc0053b2c9c9d11b594b672a77428fe632696
lyzsk <sichu.huang2021@outlook.com>
2022-03-27 10:46:50 +0100

    v5.0 EnableScheduling

---
+++ src/main/java/cn/sichu/graduatemall/config/SpringTaskConfig.java
@@ -0,0 +1,14 @@
+package cn.sichu.graduatemall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 定时任务配置
+ * 
+ * @author sichu
+ * @date 2022/03/27
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {}
